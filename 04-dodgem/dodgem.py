9e701322a8a443e345f299bbc9aa882e0cd505e9
Ruben38Esteves <ruben38esteves@gmail.com>
2025-09-24 16:16:53 +0300

    blah

--- 04-dodgem/dodgem.py
+++ 04-dodgem/dodgem.py
@@ -18,49 +18,17 @@ class GameState:
             for b in bs: 
                 b1_coords = self.board[b]
                 if b1_coords[0] != -1:
-                    # right
-                    if b1_coords[0] == 2:
-                        new_board = self.board.copy()
-                        new_board[b] = (-1,-1)
-                        substates.append(GameState(new_board,next_player,self))
-                    elif self.is_tile_free((b1_coords[0]+1,b1_coords[1])):
-                        new_board = self.board.copy()
-                        new_board[b] = (b1_coords[0]+1,b1_coords[1])
-                        substates.append(GameState(new_board,next_player,self))
-                    # down
-                    if b1_coords[1] != 2 and self.is_tile_free((b1_coords[0],b1_coords[1]+1)):
-                        new_board = self.board.copy()
-                        new_board[b] = (b1_coords[0],b1_coords[1]+1)
-                        substates.append(GameState(new_board,next_player,self))
-                    # up
-                    if b1_coords[1] != 0 and self.is_tile_free((b1_coords[0],b1_coords[1]-1)):
-                        new_board = self.board.copy()
-                        new_board[b] = (b1_coords[0],b1_coords[1]-1)
-                        substates.append(GameState(new_board,next_player,self))
+                    for valid_move in self.get_valid_moves(b):
+                        new_state = self.player_move(b, valid_move)
+                        substates.append(new_state)
         else:
             rs = ["R1","R2"]
             for r in rs:
                 r_coords = self.board[r]
                 if r_coords != (-1,-1):
-                    # up
-                    if r_coords[1] == 0:
-                        new_board = self.board.copy()
-                        new_board[r] = (-1,-1)
-                        substates.append(GameState(new_board,next_player,self))
-                    elif self.is_tile_free((r_coords[0], r_coords[1]-1)):
-                        new_board = self.board.copy()
-                        new_board[r] = (r_coords[0],r_coords[1]-1)
-                        substates.append(GameState(new_board,next_player,self))
-                    # left
-                    if r_coords[0] != 0 and self.is_tile_free((r_coords[0]-1, r_coords[1])):
-                        new_board = self.board.copy()
-                        new_board[r] = (r_coords[0]-1,r_coords[1])
-                        substates.append(GameState(new_board,next_player,self))
-                    # right
-                    if r_coords[0] != 2 and self.is_tile_free((r_coords[0]+1, r_coords[1])):
-                        new_board = self.board.copy()
-                        new_board[r] = (r_coords[0]+1,r_coords[1])
-                        substates.append(GameState(new_board,next_player,self))
+                    for valid_move in self.get_valid_moves(r):
+                        new_state = self.player_move(r, valid_move)
+                        substates.append(new_state)
         return substates
 
     def get_valid_moves(self,car):
@@ -100,17 +68,31 @@ class GameState:
     def minimax(self, gs, i, d=0, vis_score=None):
 
         if gs in vis_score:
+            score = self.get_score()
+            if gs.current_player == "B":
+                score = score - d
+            else:
+                score = score + d
+            if score < vis_score[gs]:
+                vis_score[gs] = score
             return (vis_score[gs], gs)
 
         result = gs.is_game_over()
         if result[0]:
-            score = result[1]-d if result[1] > 0 else result[1]+d
+            score = result[1]
+            if gs.current_player == "B":
+                score = score
+            else:
+                score = score
             vis_score[gs] = score
             return (score, gs)
 
-        if d > 5:
+        if d >= 7:
             score = self.get_score()
-            score = score-d if score > 0 else score+d
+            if gs.current_player == "B":
+                score = score - d
+            else:
+                score = score + d
             vis_score[gs] = score
             return (score, gs)
 
@@ -136,7 +118,8 @@ class GameState:
            coord = (-1,-1)
        new_board[car] = coord
        next_player: str = "R" if self.current_player == "B" else "B"
-       return GameState(new_board, next_player, self)
+       new_state = GameState(new_board, next_player, self)
+       return new_state
 
     def output(self, i=0):
         inital_board = [["  ","  ","  "],["  ","  ","  "],["  ","  ","  "]]
@@ -153,14 +136,32 @@ class GameState:
 
     def get_score(self):
         score = 0
+        h = 5
         if self.board["B1"] == (-1,-1):
-            score += 10
+            score += 15
+        else:
+            score += self.board["B1"][0] * h
+
         if self.board["B2"] == (-1,-1):
-            score += 10
+            score += 15
+        else:
+            score += self.board["B2"][0] * h
+
         if self.board["R1"] == (-1,-1):
-            score -= 10
+            score -= 15
+        else:
+            score -= (2 - self.board["R1"][1]) * h
+
         if self.board["R2"] == (-1,-1):
-            score -= 10
+            score -= 15
+        else:
+            score -= (2 - self.board["R2"][1]) * h
+        score += 0.5 * sum(len(self.get_valid_moves(b))
+                       for b in ["B1", "B2"]
+                       if self.board[b] != (-1,-1))
+        score -= 0.5 * sum(len(self.get_valid_moves(r))
+                       for r in ["R1", "R2"]
+                       if self.board[r] != (-1,-1))
         return score
 
     def is_game_over(self):
@@ -169,6 +170,7 @@ class GameState:
         if self.board["R1"] == (-1,-1) and self.board["R2"] == (-1,-1):
             return (True, -99)
         return (False, 0)
+
     def __eq__(self, other):
         return (
             isinstance(other, GameState)
